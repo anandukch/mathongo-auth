93fe8cb5845f195e9ee179b6eff1dc16d819b88d
anandukch <anandukch@gmail.com>
2022-05-31 13:21:48 +0530

    code formatted

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,7 +1,6 @@
 const nodemailer = require("nodemailer");
 const { google } = require("googleapis");
 const MailComposer = require("nodemailer/lib/mail-composer");
-const OAuth2 = google.auth.OAuth2;
 
 const getGmailService =async () => {
   const oAuth2Client = new google.auth.OAuth2(process.env.CLIENT_ID,
@@ -52,57 +51,6 @@ const sendMail = async (options) => {
 };
 const sendEmail = async (email, subject, text) => {
   try {
-    // const transporter = nodemailer.createTransport({
-    //   // host: "smtp.ethereal.email",
-    //   service:"gmail",
-    //   // port: 587,
-    //   // secure: false,
-    //   auth: {
-    //     type: "OAuth2",
-    //     user: process.env.USER,
-    //     pass: process.env.PASS,
-    //     clientId: process.env.CLIENT_ID,
-    //     clientSecret: process.env.CLIENT_SECRET,
-    //     refreshToken: process.env.REFRESH_TOKEN
-    //   },
-    //   tls: {
-    //     rejectUnauthorized: false
-    //   }
-    // });
-
-    // const oauth2Client = new OAuth2(
-    //   process.env.CLIENT_ID,
-    //   process.env.CLIENT_SECRET,
-    //   "https://developers.google.com/oauthplayground"
-    // );
-    
-    // oauth2Client.setCredentials({
-    //   refresh_token: process.env.REFRESH_TOKEN
-    // });
-  
-    // const accessToken = await new Promise((resolve, reject) => {
-    //   oauth2Client.getAccessToken((err, token) => {
-    //     if (err) {
-    //       reject("Failed to create access token :(");
-    //     }
-    //     resolve(token);
-    //   });
-    // });
-    // console.log(accessToken);
-    // const transporter = nodemailer.createTransport({
-    //   service: "gmail",
-    //   auth: {
-    //     type: "OAuth2",
-    //     user: process.env.USER,
-    //     accessToken,
-    //     clientId: process.env.CLIENT_ID,
-    //     clientSecret: process.env.CLIENT_SECRET,
-    //     refreshToken: process.env.REFRESH_TOKEN
-    //   },
-    //   tls: {
-    //     rejectUnauthorized: false
-    //   }
-    // });
     const messageId = await sendMail({
       from: "admin@mathongo.in",
       to: email,
@@ -110,14 +58,6 @@ const sendEmail = async (email, subject, text) => {
       text: text,
     });
   return messageId;
-    // await transporter.sendMail({
-    //   from: "admin@mathongo.in",
-    //   to: process.env.USER,
-    //   subject: subject,
-    //   text: text,
-    // });
-
-    // console.log("email sent sucessfully");
   } catch (error) {
     console.log(error);
     throw new Error(error);
